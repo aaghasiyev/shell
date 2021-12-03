f602ec03fa259b81a5edc937b861e7da89892fa8
Azar Aghasyev <azar.aghasiyev@gmail.com>
2021-12-03 08:35:18 -0800

    Python main file

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import date class from datetime module
+from datetime import date
+
+print("Hello from python tp circle ")  
+  
+# Returns the current local date
+today = date.today()
+print("Today date is: ", today)
