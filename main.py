dbaf098fbdf2a04c8b913544422c8c9dd32b8dcb
Azar Aghasyev <azar.aghasiyev@gmail.com>
2021-12-03 08:44:13 -0800

    Main py changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Import date class from datetime module
 from datetime import date
+import os
 
 print("Hello from python tp circle ")  
   
 # Returns the current local date
 today = date.today()
 print("Today date is: ", today)
+print(os.environ["CIRCLE_PROJECT_USERNAME"])
